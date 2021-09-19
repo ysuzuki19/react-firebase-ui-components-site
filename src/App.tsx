b25e85dcf4a6b0ed0ef781a894a411083a90e431
ysuzuki19 <ysuzuki.cpp@gmail.com>
2021-09-19 23:46:40 +0900

    add firebase and analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics, logEvent } from 'firebase/analytics';
 
-const App: React.FC = () => {
+import firebaseConfig from './firebase.config';
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const App = (): JSX.Element => {
+  useEffect(() => {
+    logEvent(analytics, 'visit_app');
+  }, []);
+
   return <>React Firebase ui-components site</>;
 };
 
---
+++ src/firebase.config.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDJvFl1l28K5UFAgLwALrR-dF84mNY4aQw',
+  authDomain: 'react-firebase-ui-components.firebaseapp.com',
+  projectId: 'react-firebase-ui-components',
+  storageBucket: 'react-firebase-ui-components.appspot.com',
+  messagingSenderId: '781911517906',
+  appId: '1:781911517906:web:722986ebaa42c562dd1b0b',
+  measurementId: 'G-Z6F1EZNRLF',
+};
+
+export default firebaseConfig;
